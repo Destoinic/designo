c363e81d907d38ba586966539cbe204160e58dc3
Marin-Andrei <andreimarinjunior@gmail.com>
2024-09-30 23:07:14 +0300

    tablet responsive home page

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -45,6 +45,7 @@ const Home = () => {
             </div>
           </div>
         </header>
+
         <section className="section-design">
           <div className="container-design">
             <ul>
@@ -70,44 +71,6 @@ const Home = () => {
           </div>
         </section>
         <QualitiesList />
-        {/* <section className="section-qualities">
-          <div className="qualities">
-            <img
-              src="/assets/home/desktop/illustration-passionate.svg"
-              alt="passionate-image"
-            />
-            <h3>PASSIONATE</h3>
-            <p>
-              Each project starts with an in-depth brand research to ensure we
-              only create products that serve a purpose. We merge art, design,
-              and technology into exciting new solutions.
-            </p>
-          </div>
-          <div className="qualities">
-            <img
-              src="/assets/home/desktop/illustration-resourceful.svg"
-              alt="resourceful-image"
-            />
-            <h3>RESOURCEFUL</h3>
-            <p>
-              Everything that we do has a strategic purpose. We use an agile
-              approach in all of our projects and value customer collaboration.
-              It guarantees superior results that fulfill our clients’ needs.
-            </p>
-          </div>
-          <div className="qualities">
-            <img
-              src="/assets/home/desktop/illustration-friendly.svg"
-              alt="friendly-image"
-            />
-            <h3>FRIENDLY</h3>
-            <p>
-              Friendly We are a group of enthusiastic folks who know how to put
-              people first. Our success depends on our customers, and we strive
-              to give them the best experience a company can provide.
-            </p>
-          </div>
-        </section> */}
         <SectionContact />
       </div>
       <Footer />
--- src/components/Quality.jsx
+++ src/components/Quality.jsx
@@ -1,15 +1,17 @@
 import "../styling/Quality.css";
 
-const Quality = ({ backgroundURL, title, body }) => {
-  return (
-    <div className="quality">
+const Quality = ({ backgroundURL, title, body }) => (
+  <div className="quality">
+    {/* className added to div */}
+    <div className="content">
+      <img src={backgroundURL} alt="" />
+      {/* i need to correct this in pc width */}
       <div>
-        <img src={backgroundURL} alt="" />
         <h3>{title}</h3>
         <p>{body}</p>
       </div>
     </div>
-  );
-};
+  </div>
+);
 
 export default Quality;
